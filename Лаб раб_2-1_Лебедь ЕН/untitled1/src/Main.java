85f3ccf19b4ffebb7256b5d5670a5c1989a8c7f4
Elena Lebed <111484478+lebedlena@users.noreply.github.com>
2022-08-21 09:31:04 +0300

    Add files via upload

---
+++ Лаб раб_2-1_Лебедь ЕН/untitled1/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет! Угадай число!");
+        System.out.println("Введи однозначное число и нажми ENTER");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        while (a!=7) {
+            a = scan.nextInt();
+            if (a<7) System.out.println("Не верно. Выбери число больше.");
+
+            else if (a>7) System.out.println("Попробуй ещё раз. Загаданное число меньше.");
+
+            else System.out.println("Bingo!");
+        }
+        System.out.println(a+ " "+"Поздравляю! Задание выполнено!");}
+}
